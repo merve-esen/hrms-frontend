18df112f7ca695f6d93042f43526e620d9df4c8f
merve-esen <merveesen834@gmail.com>
2021-06-24 23:15:01 +0300

    Java&React Camp - Day 14 - Homework 1

---
+++ src/pages/JobAdvertisementAdd.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function JobAdvertisementAdd() {
+  const initialValues = {
+    jobDescription: "",
+    numberOfOpenPositions: 0,
+    minmumSalary: 0,
+    maximumSalary: 0,
+    applicationDeadline: "",
+  };
+
+  return <div></div>;
+}
