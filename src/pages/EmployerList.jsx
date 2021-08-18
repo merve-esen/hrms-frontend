293b028a343a2b8782563d2dc5c4f1b2a28b22ba
merve-esen <59421135+merve-esen@users.noreply.github.com>
2021-08-18 16:39:02 +0300

    Employer updated

--- src/pages/EmployerList.jsx
+++ src/pages/EmployerList.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table, Header, Icon, Button } from 'semantic-ui-react'
+import { toast } from "react-toastify";
+import EmployerService from '../services/employerService';
 
 export default function EmployerList() {
+
+    const [employers, setEmployers] = useState([]);
+    useEffect(() => {
+        let employerService = new EmployerService();
+        employerService
+            .getAll()
+            .then((result) => setEmployers(result.data.data));
+    }, []);
+
+    const handleConfirmEmployer = (employer) => {
+        //dispatch(applyJobAdvertisement(jobAdvertisement));
+        toast.success(`${employer.companyName} şirketi onaylandı!`)
+    };
+
     return (
         <div>
-            Employer List
+            <Header as='h2' color='olive'>
+                <Icon name='address card outline' />
+                <Header.Content>İşverenler</Header.Content>
+            </Header>
+            <Table color='olive' key='olive' inverted>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Şirket Adı</Table.HeaderCell>
+                        <Table.HeaderCell>Web Sitesi</Table.HeaderCell>
+                        <Table.HeaderCell>Telefon</Table.HeaderCell>
+                        <Table.HeaderCell>Onay Durumu</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {employers.map((employer) => (
+                        <Table.Row key={employer.id}>
+                            <Table.Cell>{employer.companyName}</Table.Cell>
+                            <Table.Cell>{employer.webSite}</Table.Cell>
+                            <Table.Cell>{employer.phoneNumber}</Table.Cell>
+                            <Table.Cell>
+                                <Button color='white' inverted onClick={() => handleConfirmEmployer(employer)}>Onayla</Button>
+                            </Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
         </div>
     )
 }
